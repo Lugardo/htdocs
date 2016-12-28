028ac53f13f877473c5f1ea42b3573b74a869d41
Lugardo <yosoylugardo@hotmail.com>
2016-12-28 11:33:15 -0700

    Add files via upload

---
+++ MayorQueDos.py
@@ -0,0 +1,6 @@
+N1 = raw_input ("Dame el valor del primer numero: ")
+N2 = raw_input ("Dame el valor del segundo numero: ")
+if N1 > N2:
+	print str(N1)+ "es mayor que "+ str (N2)
+else:
+	print str(N2)+ "es mayor que "+ str (N1)
